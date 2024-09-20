396b322557b7b378208229025e97c9279546f268
Jelita02 <149029048+Jelita02@users.noreply.github.com>
2024-09-20 09:49:59 +0700

    menambahkan tabel migrasi biodata

--- database/migrations/2024_09_19_063610_create_biodatas_table.php
+++ database/migrations/2024_09_19_063610_create_biodatas_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // menambahkan table biodataaa
         Schema::create('biodatas', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
